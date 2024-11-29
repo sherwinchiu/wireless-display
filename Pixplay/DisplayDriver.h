2da5001c06a73614adbd24e163bd5400ef3b855d
Sherwin Chiu <sherwin.chiu89@gmail.com>
2024-11-29 15:50:59 -0500

    FINALLY IT DISPLAYS!

---
+++ Pixplay/DisplayDriver.h
@@ -0,0 +1,28 @@
+// For TFT Display
+#include "Arduino.h"
+#include <SPI.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+
+#define TFT_CS         1
+#define TFT_RST        3                                            
+#define TFT_DC         2
+#define TFT_BL         7
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+#define MAX_IMAGE_WIDTH 240
+#define MAX_IMAGE_HEIGHT 320
+int16_t xpos = 0;
+int16_t ypos = 0;
+
+// uint16_t imageBuffer[MAX_IMAGE_WIDTH * MAX_IMAGE_HEIGHT] PROGMEM;
+
+void setupTFT(){
+  Serial.println(F("Hello! ST77xx TFT Test"));
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  tft.init(MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT);           // Init ST7789 320x240
+  tft.fillScreen(ST77XX_BLACK);
+}
+
--- Pixplay/Pixplay.ino
+++ Pixplay/Pixplay.ino
@@ -2,10 +2,21 @@
 #include "WiFi.h"
 #include "Preferences.h"
 #include "html-file.h"
+#include "DisplayDriver.h"
 // For Webserver
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+// For getting the fil, need to setup secure client
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <NetworkClientSecure.h>
 
+// Server Name
+String serverName = "https://pixplay-540390702710.us-central1.run.app/bmp";
+
+// Update time for display set at 30min
+#define uS_TO_S_FACTOR 1000000  
+#define TIME_TO_SLEEP 600
 
 // for access point mode
 const char* ap_ssid = "PixPlay";
@@ -23,6 +34,9 @@ Preferences preferences;
 // WiFi server
 AsyncWebServer server(80);
 
+#define uS_TO_S_FACTOR 60000000  /* Conversion factor for micro seconds to minutes */
+#define TIME_TO_SLEEP  1 // minutes
+
 void setup() {
   Serial.begin(115200);
   preferences.begin("creds", false);
@@ -39,7 +53,7 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) { // Check WiFi status every 500ms. If after 7.5 attempts, no connection 
     delay(500);
     Serial.print(".");
-    if (millis() - currTime > 7500) { // Start WiFi in access point mode.
+    if (millis() - currTime > 15000) { // Start WiFi in access point mode.
       // If it doesn't exist, run server in access point mode. Wait until "submit" button is pressed and valid
       // credentials is put in. Attempt to connect, if connection isn't possible keep waiting.
       Serial.println("\nNo WiFi connection could be made. Started AP Mode. Please connect to PixPlay on your phone.");
@@ -71,7 +85,7 @@ void setup() {
       }); 
       server.begin(); // Start the server
       Serial.println("Server Started");
-      break;
+      while(true){}
     }
   }
   // Check in Cache if any WiFi settings already exist
@@ -79,9 +93,77 @@ void setup() {
   // If connection is unsucessful, skip to next step.
   Serial.println();
   Serial.println("I'm connected to WiFi!");
+
+  // Display code & Fetch Image
+  // Setup TFT
+  setupTFT();
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  //
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // ok i finished coding everything above
+  HTTPClient http;
+  http.begin(serverName);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    WiFiClient* stream = http.getStreamPtr();
+    size_t contentLength = http.getSize();
+
+    // Buffer for one line of pixels (240 pixels × 2 bytes per pixel)
+    const size_t lineWidth = 240 * 3; // 480 bytes
+    uint16_t lineBuffer[240];
+
+    size_t bytesRead = 0;
+    size_t totalBytesRead = 0;
+    int y = 0;
+    size_t skippedBytes = 0;
+    size_t headerBytes = 56;
+    while (skippedBytes < headerBytes) {
+        stream->read();  // Read and discard one byte at a time
+        skippedBytes++;
+    }
+    tft.startWrite(); // Optimize for batch rendering
+
+    // Read the image data line by line
+    while (totalBytesRead < contentLength && y < 320) {
+        // Read one line of data
+        bytesRead = stream->readBytes((char*)lineBuffer, lineWidth);
+
+        if (bytesRead > 0) {
+            // Ensure that we read the full line (if bytesRead is less than expected, we may have to wait)
+            while (bytesRead < lineWidth) {
+                delay(1);  // Wait a bit before attempting to read more bytes
+                bytesRead += stream->readBytes((char*)lineBuffer + bytesRead, lineWidth - bytesRead);
+            }
+
+            // Set the window for the current line
+            tft.setAddrWindow(0, y, 239, y);
+
+            // Write the pixel data directly to the display
+            tft.writePixels(lineBuffer, 240);
+
+            totalBytesRead += bytesRead;
+            y++;
+
+            // Delay a little before reading the next line, to slow things down
+            // delay(30); // Adjust delay to slow down the display process
+        } else {
+            Serial.println("Stream read failed or no more data!");
+            break;
+        }
+    }
+    
+    WiFi.disconnect();
+    // esp_deep_sleep_start();
+    delay(1000000);
+    ESP.restart();
+    tft.endWrite(); // End batch rendering
+  
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  Serial.println("no crash here");
+  
+  http.end();
+
 }
